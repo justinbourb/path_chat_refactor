346adf4ac491f8f114fb256a9c74b75ae6d73bae
a <justinbourb@gmail.com>
2018-03-30 16:46:38 -0400

    added base.html to chat

--- routes.py
+++ routes.py
@@ -5,7 +5,7 @@ from flask import render_template
 
 @app.route('/chat')
 def chat():
-    return render_template('chat.html', async_mode=socketio.async_mode)
+    return render_template('chat.html', async_mode=socketio.async_mode, current_user='user')
 
 @app.route('/', methods=['GET', 'POST'])
 @app.route('/index', methods=['GET', 'POST'])
