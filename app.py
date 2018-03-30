3a4a0c7faae705afca29a7f0f298f353caced229
a <justinbourb@gmail.com>
2018-03-30 16:41:02 -0400

    reintroduced index page

--- app.py
+++ app.py
@@ -2,6 +2,7 @@
 from threading import Lock
 from flask import Flask
 from flask_socketio import SocketIO
+from flask_bootstrap import Bootstrap
 
 async_mode = None
 
@@ -10,6 +11,7 @@ app.config['SECRET_KEY'] = 'secret!'
 socketio = SocketIO(app, async_mode=async_mode)
 thread = None
 thread_lock = Lock()
+bootstrap = Bootstrap(app)
 
-from routes import index
+from routes import *
 from websockets import *
--- routes.py
+++ routes.py
@@ -3,6 +3,25 @@ from app import app, socketio
 from flask import render_template
 
 
-@app.route('/')
+@app.route('/chat')
+def chat():
+    return render_template('chat.html', async_mode=socketio.async_mode)
+
+@app.route('/', methods=['GET', 'POST'])
+@app.route('/index', methods=['GET', 'POST'])
+#@login_required
 def index():
-    return render_template('index.html', async_mode=socketio.async_mode)
+    ''' nothing here yet'''
+    return render_template('index.html', title='Home', current_user='user')
+
+@app.route('/edit_profile')
+def edit_profile():
+    return ("profile page")
+
+@app.route('/login')
+def login():
+    return ("login page")
+
+@app.route('/logout')
+def logout():
+    return ("logout page")
